d0e147ae31a23b180af4f949e0a1db1f8ab85a3b
Collins Kimutai <collinskimutai72@gmail.com>
2023-03-24 15:54:27 +0300

    update

--- app.py
+++ app.py
@@ -13,15 +13,7 @@ def init():
 
     model_file = "pytorch_weights.onnx"
     model = onnxruntime.InferenceSession(model_file)
-    global transform = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize(
-            mean=[0.485, 0.456, 0.406],
-            std=[0.229, 0.224, 0.225]
-        )
-])
+    
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
@@ -31,7 +23,15 @@ def inference(model_inputs):
 
     # Convert the image data to a PIL Image object
     image = Image.open(io.BytesIO(image_data))
-
+    transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
+    ])
     # Apply the image transform and convert to a numpy array
     image_tensor = transform(image).unsqueeze(0).numpy()
 
